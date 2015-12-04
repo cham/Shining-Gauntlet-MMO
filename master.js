2cbf40c786d005cf8d009c18db55c3ba873fdd97
Dan Neame <daniel@brandwatch.com>
2015-12-04 15:15:14 +0000

    tidied up master a little

--- master.js
+++ master.js
@@ -2,32 +2,24 @@
 
 const cluster = require('cluster');
 const config = require('./lib/appConfig');
+const numToBoot = config.processes;
+let numBooted = 0;
 
-function startInitial() {
-    const numToBoot = config.processes;
+function increment(){
+    numBooted += 1;
 
-    let numBooted = 0;
-
-    function increment(){
-        numBooted += 1;
-
-        if(numBooted === numToBoot){
-            console.info(`${numToBoot} workers listening on port ${config.appPort}.`);
-        }
-    }
-
-    for(let i = 0; i < numToBoot; i += 1){
-        cluster.fork().once('listening', increment);
+    if(numBooted === numToBoot){
+        console.info(`${numToBoot} workers listening on port ${config.appPort}.`);
     }
 }
 
-startInitial();
-
-function handleExit(worker) {
-    console.error(`Worker died. PID: ${worker.process.pid}`);
-
-    const newWorker = cluster.fork().once('listening', () => console.info(`Replacement worker spawned. PID: ${newWorker.process.pid}`));
+for(let i = 0; i < numToBoot; i += 1){
+    cluster.fork().once('listening', increment);
 }
 
 // Restart dead workers
-cluster.on('exit', handleExit);
+cluster.on('exit', (worker) => {
+    console.error(`Worker died. PID: ${worker.process.pid}`);
+
+    const newWorker = cluster.fork().once('listening', () => console.info(`Replacement worker spawned. PID: ${newWorker.process.pid}`));
+});
